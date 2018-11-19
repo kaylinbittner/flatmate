d17cb540bd85ee381172fa29bae82cfbd24ef8ad
Fritz Meyer <meyerfr@gmx.de>
2018-11-19 17:45:11 +0100

    added flats_params

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,15 @@ class FlatsController < ApplicationController
   def home
 
   end
+
+  def show
+    @flat = Flat.find(params(:id))
+  end
+
+  private
+
+  def flat_params
+    params.require(:cocktail).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability)
+  end
 end
+
