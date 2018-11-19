b10ee150a702963fccab8928a9a597088f60e5aa
Fritz Meyer <meyerfr@gmx.de>
2018-11-19 15:54:10 +0100

    set up controllers and login

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,5 @@
+class FlatsController < ApplicationController
+  def home
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'flats#home'
+  resources :flats do
+    resources :bookings, only: [:new, :show, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
