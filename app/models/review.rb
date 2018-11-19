2ab156584dbb06fa2a8c3fe5cc4fb9274e99dffd
Fritz Meyer <meyerfr@gmx.de>
2018-11-19 16:12:46 +0100

    delted relationship through reviews and flats from reviews side of view

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  belongs_to :flat, through: :booking
   validates :rating, presence: true, inclusion: { in: %w(1 2 3 4 5) }
   validats :content, length: { in: 20..100 }
 end
