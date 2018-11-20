29747ac78ebf92f7d810c939a2fb7ddba8534165
Kaylin Bittner <kaylinbittner@gmail.com>
2018-11-20 10:34:08 +0100

    FOOTER & BANNER - CARDS IN PROGRESS

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,16 +1,21 @@
 class FlatsController < ApplicationController
   def home
-
+    @flats = Flat.all
   end
 
   def show
     @flat = Flat.find(params(:id))
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+  end
+
   private
 
   def flat_params
-    params.require(:cocktail).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability)
+    params.require(:flat).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability)
   end
 end
 
