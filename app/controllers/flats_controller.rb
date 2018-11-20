a9a9f794f359fdc80483b82950f350d4ab8c258c
elena tamayo <elena.tamayo@sciencespo.fr>
2018-11-20 16:45:30 +0100

    edit, update

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,74 +6,44 @@ class FlatsController < ApplicationController
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
     @flat = Flat.new
   end
 
-  def edit
-  end
-
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
-<<<<<<< HEAD
-
-    if @flat.save
-      redirect_to @flat, notice: 'Your flat was successfully created.'
-=======
-    if @flat.save!
-      redirect_to @flat
->>>>>>> 629e631d084e1f037df0a61bf7d1d416cacae8ae
-    else
-      render :new
-    end
   end
 
-<<<<<<< HEAD
-
-# <<<<<<< HEAD
-#   def create
-#     @flat = Flat.new(flat_params)
-#     if @flat.save
-#       redirect_to @
-#     else
-#       render 'new'
-# =======
-#   def new
-#     @flat = Flat.new
-#   end
-
-#   def create
-#     @flat = Flat.new(flat_params)
-#     if @flat.save
-#       redirect_to @flat
-#     else
-#       render :new
-# >>>>>>> 2dd96f64a8960289b8a397641bac28ae0932d5d6
-#     end
-
-def update
-  if false #@flat.update(flat_params)
-    redirect_to @flat, notice: 'The flat was successfully updated.'
-  else
-    render :edit
+  def edit
+    puts "hello from edit"
   end
-end
 
+  def update
+    @flat.update(flat_params)
+    redirect_to @flat
+  end
 
-private
+  def destroy
+    @flat.destroy
+  end
 
-def flat_params
-  params.require(:flat).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability, {pictures: []})
-end
-
-=======
   private
+
   def flat_params
-    params.require(:flat).permit(:description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability, {pictures: []})
+    params.require(:flat).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability, {pictures: []})
   end
->>>>>>> 629e631d084e1f037df0a61bf7d1d416cacae8ae
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+# =======
+#   private
+#   def flat_params
+#     params.require(:flat).permit(:description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability, {pictures: []})
+#   end
+# >>>>>>> 629e631d084e1f037df0a61bf7d1d416cacae8ae
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,7 +1,7 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_many :reviews, through: :bookings
-  validates :description, :price, :city, :zipcode, :street, :pictures, presence: true
+  validates :description, :price, :city, :zipcode, :street, presence: true
   validates :key_description, length: { in: 3..35 }
   validates :description, length: { minimum: 150 }
   mount_uploaders :pictures, PictureUploader
