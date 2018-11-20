cfa306b99dbd939b5840d601e527cbca9f20188c
elena tamayo <elena.tamayo@sciencespo.fr>
2018-11-20 14:36:27 +0100

    create form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,6 +7,24 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params(:id))
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+
+    if @flat.save
+      redirect_to @flat, notice: 'Your flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
 
 # <<<<<<< HEAD
 #   def create
@@ -29,23 +47,20 @@ class FlatsController < ApplicationController
 # >>>>>>> 2dd96f64a8960289b8a397641bac28ae0932d5d6
 #     end
 
-  def update
-    if @flat.update(flat_params)
-      redirect_to @flat, notice: 'The flf was successfully updated.'
-    else
-      render :edit
-    end
+def update
+  if false #@flat.update(flat_params)
+    redirect_to @flat, notice: 'The flat was successfully updated.'
+  else
+    render :edit
   end
+end
 
 
+private
 
-  end
-
-  private
-
-  def flat_params
-    params.require(:flat).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability, {pictures: []})
-  end
+def flat_params
+  params.require(:flat).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability, {pictures: []})
+end
 
 end
 
