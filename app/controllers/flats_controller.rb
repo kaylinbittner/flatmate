a686076615937aa285e1ee850c59d1559690d09c
elena tamayo <elena.tamayo@sciencespo.fr>
2018-11-20 11:07:18 +0100

    form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,10 +7,20 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params(:id))
   end
 
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def flat_params
     params.require(:cocktail).permit(:key_description, :description, :price, :city, :zipcode, :street, :country, :average_rating, :wifi, :dishwasher, :allows_pets, :kitchen, :guests, :handicap, :balcony, :smoking, :tv, :availability)
   end
+
 end
 
