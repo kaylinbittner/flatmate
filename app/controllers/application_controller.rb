5e39956ed5e06bbb0254cddf96687d76a736efcb
Fritz Meyer <meyerfr@gmx.de>
2018-11-21 10:52:46 +0100

    Add avatar to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # include Pundit
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password,
+      :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password,
+      :password_confirmation, :current_password) }
+  end
   # Pundit: white-list approach.
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -12,3 +20,5 @@ class ApplicationController < ActionController::Base
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   # end
 end
+
+
