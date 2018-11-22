fdc66e5c2fe14d1b616040c66a5d98b384ad5857
Fritz Meyer <meyerfr@gmx.de>
2018-11-22 18:04:46 +0100

    uploaded pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_094404) do
+ActiveRecord::Schema.define(version: 2018_11_19_135617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,19 +46,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_094404) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "pictures"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.bigint "booking_id"
-    t.text "content"
-    t.integer "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,7 +61,6 @@ ActiveRecord::Schema.define(version: 2018_11_21_094404) do
     t.string "last_name"
     t.boolean "host"
     t.string "phone"
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -81,5 +68,4 @@ ActiveRecord::Schema.define(version: 2018_11_21_094404) do
   add_foreign_key "bookings", "flats"
   add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
-  add_foreign_key "reviews", "bookings"
 end
