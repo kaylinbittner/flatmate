960d45fa2659f80e2b59809ac1f44d2358f3fe78
Fritz Meyer <meyerfr@gmx.de>
2018-11-23 11:17:17 +0100

    added ifclause around @users_bookings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def my_profile
     @user = current_user
-    @user_bookings = current_user.bookings if current_user.bookings.exists?
+    @user_bookings = current_user.bookings if current_user.exists?
     @user_flats = current_user.flats
     @flats = Flat.all
     @users = User.all
